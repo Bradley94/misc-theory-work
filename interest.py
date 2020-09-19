46bf2ad3592448c698128da43767195aa9e6a29c
Bradley94 <60988398+Bradley94@users.noreply.github.com>
2020-09-19 12:04:45 +0100

    Create interest.py

---
+++ interest.py
@@ -0,0 +1,10 @@
+"""
+For the given amount of money, calculate the income from this savings account with a 5% interest rate after one year.
+
+Save the result into the variable income.
+"""
+amount = 1000
+interest_rate = 5
+years = 1
+# change the next line
+income = amount * ((interest_rate * years) / 100)
